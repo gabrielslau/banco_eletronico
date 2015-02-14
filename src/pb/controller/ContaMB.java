ea180b550240e779c8d4b363336bf68809e62a3a
Gabriel Lau <okatsura.lau@gmail.com>
2015-02-14 04:20:00 -0300

    organização da estrutura visual do projeto (JSF + template); implementação das funcionalidades de listar e adicionar contas;

--- src/pb/controller/ContaMB.java
+++ src/pb/controller/ContaMB.java
@@ -1,27 +1,72 @@
 package pb.controller;
 
-import javax.ejb.EJB;
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ViewScoped;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.ejb.EJB;
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+
+import pb.entity.Conta;
 import pb.service.ContaService;
 
-@ManagedBean(name="contaMB")
-@ViewScoped
+@ManagedBean(name = "contaMB")
+@ApplicationScoped
 public class ContaMB extends AppMB {
-	private static final long serialVersionUID = 5916320095618212562L;
+
+	private static final long serialVersionUID = 3925545192631139952L;
 
 	@EJB
 	private ContaService service;
 
-	public ContaMB(){
+	private List<Conta> contas;
+	private Conta conta;
+
+	public ContaMB() {
 		super();
+		contas = new ArrayList<Conta>();
+		conta = new Conta();
 	}
-	
-	private String OpDeposito(){
+
+	private String OpDeposito() {
 		return "formDeposito.xhml";
 	}
-	private String OpSaque(){
+
+	private String OpSaque() {
 		return "formSaque.xhml";
+	}
+
+	public void add() {
+		if (service.save(conta) != null) {
+			System.err.println(" salvou ");
+			this.redirect("contas/index");
+		} else {
+			this.redirect("contas/add");
+		}
+	}
+
+	public Conta getConta() {
+		return conta;
+	}
+
+	public void setConta(Conta conta) {
+		this.conta = conta;
+	}
+
+	public List<Conta> getContas() {
+		contas = service.findAll();
+		return contas;
+	}
+
+	public void setContas(List<Conta> contas) {
+		this.contas = contas;
+	}
+
+	public boolean isVazio() {
+		return this.getContas().isEmpty();
+	}
+
+	public boolean isNotVazio() {
+		return !this.isVazio();
 	}
 }
--- src/pb/fachada/BancoEletronicoFachadaImpl.java
+++ src/pb/fachada/BancoEletronicoFachadaImpl.java
@@ -8,21 +8,26 @@ import javax.ejb.TransactionManagement;
 import javax.ejb.TransactionManagementType;
 
 import pb.entity.Conta;
+import pb.exception.DinheiroInsuficienteException;
 import pb.exception.OperacaoInvalidaException;
 import pb.service.ContaService;
 
 @Stateless
-@TransactionManagement(value=TransactionManagementType.BEAN)
+@TransactionManagement(value=TransactionManagementType.CONTAINER)
 public class BancoEletronicoFachadaImpl implements BancoEletronicoFachada {
 	@EJB
 	private ContaService contaService;
 
+	// TODO: saber como abortar a transação
 	@Override
 	@TransactionAttribute(TransactionAttributeType.REQUIRED)
-	public boolean transferir(double quanto, Conta deQualConta,
-			Conta paraQualConta) throws OperacaoInvalidaException {
-		// TODO Auto-generated method stub
-
-		return false;
+	public boolean transferir(double quanto, Conta deQualConta, Conta paraQualConta) throws OperacaoInvalidaException {
+		try{
+			contaService.withdraw(deQualConta, quanto);
+			contaService.deposit(paraQualConta, quanto);
+		}catch(DinheiroInsuficienteException e){
+			return false;
+		}
+		return true;
 	}
 }
